4abbc228e1fb19bb8d5f04309afff80e34ecdb0c
altugkarakurt <altugkarakurt@gmail.com>
2025-05-24 15:41:20 -0600

    Added pagination

--- blog/views.py
+++ blog/views.py
@@ -20,6 +20,7 @@ class PostListView(ListView):
     model = Post
     template_name = "blog/blog_index.html"
     ordering = ["-date_posted"]
+    paginate_by = 10
 
 
 class PostDetailView(DetailView):
