b61e29ba643f2a8d5f6a69a11486dbd011285810
altugkarakurt <altugkarakurt@gmail.com>
2025-05-24 21:45:13 -0600

    Adding visitor tracker

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
+    "user_visit",
 ]
 
 MIDDLEWARE = [
@@ -34,6 +35,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "user_visit.middleware.UserVisitMiddleware",
 ]
 
 ROOT_URLCONF = "personal_website.urls"
